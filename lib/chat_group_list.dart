97a1781e7e6f5d22cb6e2d7b805136daf154e8c0
urijkotelnikov <yk@ekb-app.ru>
2024-07-07 13:07:11 +0500

    Initial commit

---
+++ lib/chat_group_list.dart
@@ -0,0 +1,17 @@
+library chat_group_list;
+
+import 'package:flutter/material.dart';
+
+class ChatGroupList extends StatefulWidget {
+  const ChatGroupList({super.key});
+
+  @override
+  State<ChatGroupList> createState() => _ChatGroupListState();
+}
+
+class _ChatGroupListState extends State<ChatGroupList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
